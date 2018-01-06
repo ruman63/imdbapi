0448791ad3294e99b8477ab77286a4a42f9ae300
Ruman Saleem <ruman63@gmail.com>
2018-01-06 18:45:40 +0530

    Responsive search UI

--- app/views/layouts/nav.view.php
+++ app/views/layouts/nav.view.php
@@ -3,20 +3,18 @@
         <div class="nav-wrapper">
             <a href="/" class="brand-logo center">Open Movie Database App</a>
         </div>
-        <div class="nav-content hide-on-small-only">
-            <form class="search-wrapper card" action="/">
-                <div class="level v-centered black-text">
-                    <input id="material-search" placeholder="Search Movies, Shows, Episodes Here ..."
-                        type="text" class="flex mr-half"
-                        name="search"
-                        value="<?= App\Core\Request::query('search') ?? '' ?>">
-                    <select name="type" id="material-search-filter" class="mr-half">
-                        <option value="all" selected>All</option>
-                        <option value="movie">Movies</option>
-                        <option value="series">TV Shows</option>
-                    </select>
-                    <button type="submit" class="white mr-half"><i class="material-icons">search</i></button>
-                </div>
+        <div class="nav-content">
+            <form class="search-wrapper card black-text" action="/">
+                <input id="material-search" placeholder="Search Movies, Shows, Episodes Here ..."
+                    type="text" class="mr-half"
+                    name="search"
+                    value="<?= App\Core\Request::query('search') ?? '' ?>">
+                <select name="type" class="mr-half">
+                    <option value="all" selected>All</option>
+                    <option value="movie">Movies</option>
+                    <option value="series">TV Shows</option>
+                </select>
+                <button type="submit" class="transparent"><i class="material-icons">search</i></button>
             </form>
         </div>
     </div>
